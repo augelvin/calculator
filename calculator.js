d27e8cdc3b5880d5354b4b6f958302c202bf03b3
Augelvin Theone Dermaga <augelvintd@gmail.com>
2023-01-23 21:45:12 -0800

    add operate function

--- calculator.js
+++ calculator.js
@@ -12,5 +12,17 @@ const multiply = function(a, b) {
 };
 
 const divide = function(a, b) {
-    return a/b
+  return a/b
 };
+
+const operate = function(op, a, b) {
+  if (op=='add') {
+    return add(a, b)
+  } else if (op=='subtract') {
+    return subtract(a, b)
+  } else if (op=='multiply') {
+    return multiply(a, b)
+  } else if (op=='divide') {
+    return divide(a, b)
+  }
+}
