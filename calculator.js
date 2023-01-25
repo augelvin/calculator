11853bec090a5063bf0a3c6118c2e6adc19bc15a
Augelvin Theone Dermaga <augelvintd@gmail.com>
2023-01-24 20:04:28 -0800

    change ID for numbers and simplify JS code

--- calculator.js
+++ calculator.js
@@ -1,55 +1,62 @@
-let displayValue = '';
+let currentValue = '';
 
-const zero = document.querySelector('#zero');
+/*
+const zero = document.querySelector('#n0');
 zero.addEventListener('click', () => {
   displayValue += '0';
 })
 
-const one = document.querySelector('#one');
+const one = document.querySelector('#n1');
 one.addEventListener('click', () => {
   displayValue += '1';
 })
 
-const two = document.querySelector('#two');
+const two = document.querySelector('#n2');
 two.addEventListener('click', () => {
   displayValue += '2';
 })
 
-const three = document.querySelector('#three');
+const three = document.querySelector('#n3');
 three.addEventListener('click', () => {
   displayValue += '3';
 })
 
-const four = document.querySelector('#four');
+const four = document.querySelector('#n4');
 four.addEventListener('click', () => {
   displayValue += '4';
 })
 
-const five = document.querySelector('#five');
+const five = document.querySelector('#n5');
 five.addEventListener('click', () => {
   displayValue += '5';
 })
 
-const six = document.querySelector('#six');
+const six = document.querySelector('#n6');
 six.addEventListener('click', () => {
   displayValue += '6';
 })
 
-const seven = document.querySelector('#seven');
+const seven = document.querySelector('#n7');
 seven.addEventListener('click', () => {
   displayValue += '7';
 })
 
-const eight = document.querySelector('#eight');
+const eight = document.querySelector('#n8');
 eight.addEventListener('click', () => {
   displayValue += '8';
 })
 
-const nine = document.querySelector('#nine');
+const nine = document.querySelector('#n9');
 nine.addEventListener('click', () => {
   displayValue += '9';
 })
-/*
+
+const point = document.querySelector('#n.');
+point.addEventListener('click', () => {
+  displayValue += '0';
+})
+
+
 const addButton = document.querySelector('#add');
 addButton.addEventListener('click', () => {
   displayValue += '0';
@@ -70,28 +77,31 @@ divideButton.addEventListener('click', () => {
   displayValue += '0';
 })
 */
-const point = document.querySelector('#point');
-point.addEventListener('click', () => {
-  displayValue += '0';
-})
+
+
+const numbers = document.querySelectorAll('.number');
+numbers.forEach((n) => {
+  n.addEventListener('click', () => {
+    currentValue += n.id.slice(-1);
+  });
+});
 
 const del = document.querySelector('#delete');
 del.addEventListener('click', () => {
-  displayValue = displayValue.slice(0, -1);
+  currentValue = displayValue.slice(0, -1);
 })
 
 const clear = document.querySelector('#clear');
 clear.addEventListener('click', () => {
-  displayValue = '';
+  currentValue = '';
 })
-
 
 const value = document.querySelector('#value');
 
 const buttons = document.querySelectorAll('.button');
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
-    value.innerHTML= displayValue;
+    value.innerHTML= currentValue;
   });
 });
 
